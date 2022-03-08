b8dd170f3f82597f1eccefa9064c0b7246c9de9d
Max Anstey <maxanstey@gn.apc.org>
2022-03-08 21:41:51 +0000

    Create TodoListItemType.php and add form to front-end

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\TodoListItem;
+use App\Form\TodoListItemType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoListController extends AbstractController
 {
     #[Route('', name: 'app_todo_list')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(TodoListItemType::class, new TodoListItem());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd(
+                $form->getData()
+            );
+        }
+
         return $this->render('todo_list/index.html.twig', [
             'controller_name' => 'TodoListController',
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/TodoListItemType.php
+++ src/Form/TodoListItemType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\TodoListItem;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,18 @@ class TodoListItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'list-group-item shadow-sm my-2 border-0',
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary ms-2',
+                    'placeholder' => 'Do the dishes',
+                ],
+                'label' => '💾',
+            ])
         ;
     }
 
