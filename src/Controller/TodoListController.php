51c3227b969331671e2ec6c573d51676de1ab96d
Max Anstey <maxanstey@gn.apc.org>
2022-03-09 21:26:53 +0000

    Remove unused import

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -8,7 +8,6 @@ use App\Form\TodoListItemType;
 use App\Repository\TodoListRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
